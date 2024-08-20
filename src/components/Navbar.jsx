74c618709c2c1847d56fc5565488a4320648edbf
itsme-boii <tushark.tec@gmail.com>
2024-08-20 19:56:01 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Nav = styled.div`
 const NavbarContainer = styled.div`
   width:100%;
   max-width: 2400px;
-  min-width: 1800px;
+
   padding: -8px;
   display: flex;
   align-items: center;
